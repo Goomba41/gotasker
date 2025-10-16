2b2b77bd7fe538a4fa085cc67dfc9cc1cab1af2c
Антон Бородавкин <borodavkin@kn-k.ru>
2025-10-16 17:11:23 +0300

    🔧 chore: Мелкие правки миграций и создание модели пользователя

--- migrations/000002_add_task.up.sql
+++ migrations/000002_add_task.up.sql
@@ -3,12 +3,12 @@ CREATE TABLE tasks (
 	title text NOT NULL,
 	description text,
   -- status text NOT NULL DEFAULT 'todo'
-  asignee_id bigint NOT NULL,
+  assignee_id bigint NOT NULL,
 	created_at TIMESTAMPTZ DEFAULT now() NOT NULL,
 	updated_at TIMESTAMPTZ DEFAULT now() NOT NULL,
 
 	CONSTRAINT tasks_pk PRIMARY KEY (id),
 	CONSTRAINT users_fk
-        FOREIGN KEY (asignee_id)
+        FOREIGN KEY (assignee_id)
         REFERENCES public.users(id)
 );
